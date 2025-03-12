8d10a78db8a5c813693c5821dd891c6599a4d991
Rohit <158751557+rohitindustry09@users.noreply.github.com>
2025-03-12 18:53:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,44 @@
+const express = require("express");
+const http = require("http");
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 let users = {}; // Store connected users with their IDs
 
 wss.on("connection", (ws) => {
     ws.on("message", (message) => {
-        const data = JSON.parse(message);
+        try {
+            const data = JSON.parse(message);
 
-        switch (data.type) {
-            case "register":
-                users[data.id] = ws;
-                console.log(`User ${data.id} connected`);
-                break;
+            switch (data.type) {
+                case "register":
+                    users[data.id] = ws;
+                    console.log(`User ${data.id} connected`);
+                    break;
 
-            case "call":
-                if (users[data.to]) {
-                    users[data.to].send(JSON.stringify({ type: "incomingCall", from: data.from, offer: data.offer }));
-                }
-                break;
+                case "call":
+                    if (users[data.to]) {
+                        users[data.to].send(JSON.stringify({ type: "incomingCall", from: data.from, offer: data.offer }));
+                    }
+                    break;
 
-            case "answer":
-                if (users[data.to]) {
-                    users[data.to].send(JSON.stringify({ type: "callAccepted", answer: data.answer }));
-                }
-                break;
+                case "answer":
+                    if (users[data.to]) {
+                        users[data.to].send(JSON.stringify({ type: "callAccepted", answer: data.answer }));
+                    }
+                    break;
 
-            case "iceCandidate":
-                if (users[data.to]) {
-                    users[data.to].send(JSON.stringify({ type: "iceCandidate", candidate: data.candidate }));
-                }
-                break;
+                case "iceCandidate":
+                    if (users[data.to]) {
+                        users[data.to].send(JSON.stringify({ type: "iceCandidate", candidate: data.candidate }));
+                    }
+                    break;
+            }
+        } catch (err) {
+            console.error("Invalid JSON received:", err);
         }
     });
 
@@ -43,4 +52,9 @@ wss.on("connection", (ws) => {
         }
     });
 });
-console.log("WebSocket server running on ws://localhost:8080");
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+                        
